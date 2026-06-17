815c219ba1d55945b1a0ff128205bcd38c665d0e
Vrushti Patel <vrushtipatel@Vrushtis-MacBook-Air.local>
2026-06-17 15:18:26 +0530

    Complete backend foundation setup

---
+++ backend/server/src/auth/dto/register.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class RegisterDto {
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  role: string;
+}
